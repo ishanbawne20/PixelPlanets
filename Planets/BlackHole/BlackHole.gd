a6bc181c8e5195aa9c6aee3a1beca058ac4f36e7
IshanBawne <ishanb20@iitk.ac.in>
2021-09-04 01:33:55 +0530

    editor star color

--- Planets/BlackHole/BlackHole.gd
+++ Planets/BlackHole/BlackHole.gd
@@ -57,4 +57,16 @@ func randomize_colors():
 	set_colors([Color("272736")] + [cols[0], cols[3]] + cols)
 
 func expCols():
-	randomize_colors()
+	var cols = {
+		"Palet 1" : [Color(230, 57, 70)/255,Color(236, 154, 154)/255,Color(241, 250, 238)/255,Color(205, 234, 229)/255,Color(168, 218, 220)/255,Color(119, 171, 189)/255,Color(69, 123, 157)/255,Color(29, 53, 87)/255],
+		"Palet 2" : [Color(216, 243, 220)/255,Color(183, 228, 199)/255,Color(149, 213, 178)/255,Color(116, 198, 157)/255,Color(82, 183, 136)/255,Color(64, 145, 108)/255,Color(27, 67, 50)/255,Color(8, 28, 21)/255],
+		"Palet 3" : [Color(0, 0, 0)/255,Color(10, 17, 31)/255,Color(20, 33, 61)/255,Color(136, 98, 39)/255,Color(252, 163, 17)/255,Color(241, 196, 123)/255,Color(229, 229, 229)/255,Color(255, 255, 255)/255],
+		"Palet 4" : [Color(255, 252, 242)/255,Color(230, 225, 214)/255,Color(204, 197, 185)/255,Color(64, 61, 57)/255,Color(51, 49, 46)/255,Color(37, 36, 34)/255,Color(136, 65, 37)/255,Color(235, 94, 40)/255],
+		"Palet 5" : [Color(61, 90, 128)/255,Color(107, 142, 173)/255,Color(152, 193, 217)/255,Color(188, 222, 235)/255,Color(224, 251, 252)/255,Color(231, 180, 165)/255,Color(238, 108, 77)/255,Color(41, 50, 65)/255],
+	}
+	randomize()
+	var nam = cols.keys()[randi()% len(cols)]
+	var col = cols[nam]
+	set_colors(col)
+	return nam
+
